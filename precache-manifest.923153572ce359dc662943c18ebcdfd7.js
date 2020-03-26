60edce4f68a454c1a438a3f8ccd83dfcf55c8602
Thoa Duong <kimthoa2598@gmail.com>
2020-03-26 14:36:09 +0700

    Add first version

---
+++ precache-manifest.923153572ce359dc662943c18ebcdfd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116acc2d171fe7d3dd500e36149555c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07da2b27bc580de7ac7",
+    "url": "/static/js/2.5204c312.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5204c312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625426579901b85b47ef",
+    "url": "/static/js/main.036da46a.chunk.js"
+  },
+  {
+    "revision": "c9d133576eb08e87b096",
+    "url": "/static/js/runtime-main.51387af1.js"
+  }
+]);
